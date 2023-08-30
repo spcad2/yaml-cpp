0670b51f67b63bafedc535bbf40cbfaa75c95bbc
Simon-Pierre Cadieux <cadiesp@NAVCANADA.CA>
2023-08-30 11:09:57 -0400

    Added 'conanfile.py'

---
+++ conanfile.py
@@ -0,0 +1,36 @@
+import os
+from conan import ConanFile
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata
+
+
+class YamlCppConan(ConanFile):
+    name = "yaml-cpp"
+    version = "0.8.0"
+    description = "A YAML parser and emitter in C++"
+    homepage = "https://github.com/jbeder/yaml-cpp"
+    url = "https://github.com/spcad2/yaml-cpp.git"
+    license = "MIT"
+
+    def export(self):
+        git = Git(self, self.recipe_folder)
+        scm_url, scm_commit = git.get_url_and_commit()
+        # we store the current url and commit in conandata.yml
+        update_conandata(self, {"sources": {"commit": scm_commit, "url": scm_url}})
+
+    def layout(self):
+        self.folders.source = "."
+
+    def source(self):
+        # we recover the saved url and commit from conandata.yml and use them to get sources
+        git = Git(self)
+        sources = self.conan_data["sources"]
+        git.clone(url=sources["url"], target=".")
+        git.checkout(commit=sources["commit"])
+
+    def build(self):
+        # build() will have access to the sources, obtained with the clone in source()
+        cmake = os.path.join(self.source_folder, "CMakeLists.txt")
+        hello = os.path.join(self.source_folder, "src/hello.cpp")
+        self.output.info("MYCMAKE-BUILD: {}".format(load(self, cmake)))
+        self.output.info("MYFILE-BUILD: {}".format(load(self, hello)))
